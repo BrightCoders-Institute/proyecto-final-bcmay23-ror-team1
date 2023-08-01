c81dfeb073de1a11e72cd377c6e8c0299340dede
JuanMichel16 <Juan.michel16@outlook.com>
2023-08-01 16:15:33 -0600

    feature: User model has been created and foreign key to user has been added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_011601) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_01_221305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_011601) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.text "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_011601) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
   add_foreign_key "users", "active_storage_attachments", column: "avatar_id"
   add_foreign_key "users", "active_storage_attachments", column: "banner_id"
 end
