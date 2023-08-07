c7d7c5bc38c4f7d9c58685d066024390e59c3ba9
Branyoe <byha010@gmail.com>
2023-08-06 18:25:53 -0600

    Fix: Commented code was removed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# # Post
-# Post.create([
-#   {
-#     content: "test post 1", 
-#     user_id: 1
-#   },
-#   {
-#     content: "test post 2", 
-#     user_id: 2
-#   },
-#   {
-#     content: "test post 3", 
-#     user_id: 3
-#   }
-# ])
+# Post
+Post.create([
+  {
+    content: "test post 1", 
+    user_id: 1
+  },
+  {
+    content: "test post 2", 
+    user_id: 2
+  },
+  {
+    content: "test post 3", 
+    user_id: 3
+  }
+])
 
-# # SharedPost
-# SharedPost.create([
-#   {
-#     user: User.first, 
-#     post: Post.second
-#   },
-#   {
-#     user: User.first, 
-#     post: Post.third
-#   },
-#   {
-#     user: User.second, 
-#     post: Post.third
-#   }
-# ])
+# SharedPost
+SharedPost.create([
+  {
+    user: User.first, 
+    post: Post.second
+  },
+  {
+    user: User.first, 
+    post: Post.third
+  },
+  {
+    user: User.second, 
+    post: Post.third
+  }
+])
