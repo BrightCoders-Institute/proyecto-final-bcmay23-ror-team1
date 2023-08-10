5bfa7b2cd879fd01fdc98c5017bc37c40c614e87
JoseGabino12 <gabinoMorales1212@hotmail.com>
2023-08-09 21:26:00 -0600

    Finished post list view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,23 +5,4 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = current_user.posts.build(post_params)
-
-    if @post.save
-      redirect_to root_path, notice: "Post created successfully!"
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:content)
-  end
 end
